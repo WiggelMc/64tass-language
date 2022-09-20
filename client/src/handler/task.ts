bba83d5914be7c2bc8cb0c8de3532cc7575967e7
WiggelMc <kim.hh@outlook.de>
2022-09-20 18:27:35 +0200

    move execution of taskRequests to server module

--- client/src/handler/task.ts
+++ client/src/handler/task.ts
@@ -1,8 +1,8 @@
 import { Task, TaskEndEvent, tasks, TaskStartEvent } from "vscode";
-import { TaskEndRequest, TaskParams, TaskResult, TaskStartRequest, TaskType } from "../common/capabilities/task";
+import { TaskParams, TaskType } from "../common/capabilities/task";
 import { ClientHandler } from "./handler";
-import { client } from "../util/languageclient";
 import { setErrorShown } from "./terminal";
+import { sendTaskEndRequest, sendTaskStartRequest } from "../server/task";
 
 export const taskHandler: ClientHandler = {
     register(context) {
@@ -23,7 +23,7 @@ async function(e) {
 	const params: TaskParams = {
 		task: e.execution.task.name
 	};
-	client.sendRequest(TaskStartRequest.method, params);
+	sendTaskStartRequest(params);
 };
 
 const onDidEndTask: (e: TaskEndEvent) => any =
@@ -37,7 +37,7 @@ async function(e) {
 	const params: TaskParams = {
 		task: e.execution.task.name
 	};
-	client.sendRequest(TaskEndRequest.method, params).then((r: TaskResult) => {
+	sendTaskEndRequest(params).then( r => {
 
 		if (r.type === TaskType.assemble || r.type === TaskType.assembleAndStart) {
 			//this might reset errorShown after running if 'assembleAndStart' is an independent task
--- client/src/server/task.ts
+++ client/src/server/task.ts
@@ -1,6 +1,16 @@
-import { TaskFetchParams, TaskFetchRequest, OptionalTaskIdentifier, TaskIdentifier } from "../common/capabilities/task";
+import { TaskFetchParams, TaskFetchRequest, OptionalTaskIdentifier, TaskIdentifier, TaskParams, TaskResult, TaskStartRequest, TaskEndRequest } from "../common/capabilities/task";
 import { client } from "../util/languageclient";
 
+
+export async function sendTaskStartRequest(params: TaskParams): Promise<TaskResult> {
+
+    return client.sendRequest(TaskStartRequest.method, params);
+}
+
+export async function sendTaskEndRequest(params: TaskParams): Promise<TaskResult> {
+
+    return client.sendRequest(TaskEndRequest.method, params);
+}
 
 export async function sendTaskFetchRequest(params: TaskFetchParams): Promise<TaskIdentifier> {
 
