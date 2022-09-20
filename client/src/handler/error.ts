22986602d3b963f60ff0da8a2929a77cc1cb609e
WiggelMc <kim.hh@outlook.de>
2022-09-20 17:49:30 +0200

    make error handling use a Map and Regex

--- client/src/handler/error.ts
+++ client/src/handler/error.ts
@@ -1,31 +1,46 @@
 import { window } from "vscode";
 
+const errorMap: Map<string, string> = new Map([
 
-export async function displayErrorMessage(error?: Error) {
-	switch (error?.message) {
-		case "Task not Found":
-			window.showErrorMessage(`Could not find Task`);
-			break;
-		case "Source File not Found":
-			window.showErrorMessage("Could not find Source File");
-			break;
-		case "List File not Found":
-			window.showErrorMessage("Could not find List File");
-			break;
-		case "Invalid Task Argument":
-			window.showErrorMessage("Argument for Custom Task is not valid");
-			break;
-		case "No Open Editor":
-			window.showErrorMessage("No Open Editor");
-			break;
-		case "Task not defined":
-			window.showErrorMessage("The requested Task is not defined for the current Directory");
-			break;
-		default:
-			if (error?.message.startsWith("cannot open")) {
-				window.showErrorMessage("Could not open File");
-			} else {
-				window.showErrorMessage(`Error while running Command: ${error?.message}`);
-			}
+	["Task not Found"        , "Could not find Task"],
+	["Source File not Found" , "Could not find Source File"],
+	["List File not Found"   , "Could not find List File"],
+	["Invalid Task Argument" , "Argument for Custom Task is not valid"],
+	["No Open Editor"        , "No Open Editor"],
+	["Task not defined"      , "The requested Task is not defined for the current Directory"],
+]);
+
+const errorRegex: [RegExp, string][] = [
+
+	[/^cannot open/         , "Could not open File"],
+];
+
+export async function displayErrorMessage(error?: any) {
+
+	if (error === undefined || !(error instanceof Error)) {
+		window.showErrorMessage("An unknown Error occurred");
+
+	} else if (errorMap.has(error.message)) {
+		window.showErrorMessage(errorMap.get(error.message));
+
+	} else {
+		const m = matchRegex(error.message);
+
+		if (m !== undefined) {
+			window.showErrorMessage(m);
+		} else {
+			window.showErrorMessage(`Error while running Command: ${error.message}`);
+		}
 	}
+}
+
+function matchRegex(message: string): string | undefined {
+
+	for (const [regex, value] of errorRegex) {
+		
+		if (regex.test(message)) {
+			return value;
+		}
+	}
+	return undefined;
 }
