eaf12c13aaf88e14d1e60e3f67855d81e5efc2f3
WiggelMc <kim.hh@outlook.de>
2022-11-13 21:16:04 +0100

    create index and listener

---
+++ server/src/util/index/file-index/file-index.ts
@@ -0,0 +1,7 @@
+export class FileIndex {
+    //Manages Files including
+    //   their dependencies/dependents
+    //   their parsed contents
+
+    // (might be split into two modules)
+}
---
+++ server/src/util/index/file-listener/file-listener-watcher.ts
@@ -0,0 +1,5 @@
+export class FileListenerWatcher {
+    //watches a directory
+    //can be turned on or off
+    //events can be subscribed to (same format as file-system)
+}
