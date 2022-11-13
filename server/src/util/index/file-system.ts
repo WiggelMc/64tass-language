f1275851ed6adfdc056dbc52637be214357be02d
WiggelMc <kim.hh@outlook.de>
2022-11-13 05:57:08 +0100

    split file-system into 3 files

--- server/src/util/index/file-system.ts
+++ server/src/util/index/file-system.ts
@@ -1,16 +1,10 @@
 import EventEmitter = require("events");
-
-type Path = FilePath | DirPath;
-type FilePath = string;
-type DirPath = string;
-
-type PathSegment = FilePathSegment | DirPathSegment;
-type FilePathSegment = string;
-type DirPathSegment = string;
+import { DirPath, File, FilePath, Path, PathSegment } from "./file";
+import { FileSystemNode } from "./file-system-node";
 
 const REMOVE_FILE_EVENT = Symbol('FileRemoved');
 
-class FileSystem {
+export class FileSystem {
     head: FileSystemNode = new FileSystemNode(this);
     eventEmitter: EventEmitter = new EventEmitter();
 
@@ -88,94 +82,4 @@ class FileSystem {
 
 function splitPath(path: Path): PathSegment[] {
     return path.split("/");
-}
-
-class FileSystemNode {
-    files: Map<string, File> = new Map();
-    children: Map<string, FileSystemNode> = new Map();
-    isTracked: boolean = false;
-    fileSystem: FileSystem;
-
-    constructor(fileSystem: FileSystem) {
-        this.fileSystem = fileSystem;
-    }
-
-    trackDir(pathSegments: DirPathSegment[]) {
-        this.createNodeAndDo(pathSegments, n => {
-            n.isTracked = true;
-        });
-    }
-    untrackDir(pathSegments: DirPathSegment[]) {
-        this.getNodeAndDo(pathSegments, (n, t) => {
-            n.isTracked = false;
-            if (!t) {
-                n.untrackFiles();
-            }
-        });
-    }
-
-    untrackFiles() {
-        if (this.isTracked) {
-            return;
-        }
-
-        for (const [segment, file] of this.files) {
-            this.fileSystem.emitFileRemoved(file);
-        }
-        this.files.clear();
-
-        for (const [segment, child] of this.children) {
-            child.untrackFiles();
-            if (child.isEmpty()) {
-                this.children.delete(segment);
-            }
-        }
-    }
-    isEmpty(): boolean {
-        return (
-            !this.isTracked 
-            && this.children.size === 0 
-            && this.files.size === 0
-        );
-    }
-
-    getNodeAndDo<R>(pathSegments: DirPathSegment[], f: (node: FileSystemNode, isTracked: boolean) => R, isTracked = false): R | undefined {
-        const segment = pathSegments.shift();
-        
-        if (segment === undefined) {
-            return f(this, isTracked);
-        }
-
-        const nextNode = this.children.get(segment);
-        const value = nextNode?.getNodeAndDo(pathSegments, f, isTracked || this.isTracked);
-        if (nextNode?.isEmpty()) {
-            this.children.delete(segment);
-        }
-        return value;
-    }
-    createNodeAndDo<R>(pathSegments: DirPathSegment[], f: (node: FileSystemNode) => R): R {
-        const segment = pathSegments.shift();
-
-        if (segment === undefined) {
-            return f(this);
-        }
-
-        const nextNode = this.createNextNode(segment);
-        return nextNode.createNodeAndDo(pathSegments, f);
-    }
-
-    createNextNode(segment: string): FileSystemNode {
-        let nextNode = this.children.get(segment);
-
-        if (nextNode === undefined) {
-            nextNode = new FileSystemNode(this.fileSystem);
-            this.children.set(segment, nextNode);
-        }
-
-        return nextNode;
-    }
-}
-
-class File {
-    //Might be removed in favour of Generic Parameter
 }
---
+++ server/src/util/index/file.ts
@@ -0,0 +1,11 @@
+export type Path = FilePath | DirPath;
+export type FilePath = string;
+export type DirPath = string;
+
+export type PathSegment = FilePathSegment | DirPathSegment;
+export type FilePathSegment = string;
+export type DirPathSegment = string;
+
+export class File {
+    //Might be removed in favour of Generic Parameter
+}
