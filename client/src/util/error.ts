94df50e445b60547d2c490ef50cbb88621f7902a
WiggelMc <kim.hh@outlook.de>
2022-11-17 22:46:08 +0100

    clean up error module

--- client/src/util/error.ts
+++ client/src/util/error.ts
@@ -1,47 +1,18 @@
 import { window } from "vscode";
 
-const errorMap: Map<string, string> = new Map([
-
-	["Task not Found", "Could not find Task"],
-	["Source File not Found", "Could not find Source File"],
-	["List File not Found", "Could not find List File"],
-	["Invalid Task Argument", "Argument for Custom Task is not valid"],
-	["No Open Editor", "No Open Editor"],
-	["Task not defined", "The requested Task is not defined for the current Directory"],
-	["Command could not be created", "The Requested Command could not be created from 64tasslang.json"],
-]);
-
-const errorRegex: [RegExp, string][] = [
-
-	[/^cannot open/, "Could not open File"],
-];
-
 export async function displayErrorMessage(error?: any) {
 
-	if (error === undefined || !(error instanceof Error)) {
-		window.showErrorMessage("An unknown Error occurred");
-
-	} else if (errorMap.has(error.message)) {
-		window.showErrorMessage(errorMap.get(error.message));
-
-	} else {
-		const m = matchRegex(error.message);
-
-		if (m !== undefined) {
-			window.showErrorMessage(m);
-		} else {
-			window.showErrorMessage(error.message);
-		}
-	}
+	const errorMessage = getErrorMessage(error);
+	window.showErrorMessage(errorMessage);
 }
 
-function matchRegex(message: string): string | undefined {
-
-	for (const [regex, value] of errorRegex) {
-
-		if (regex.test(message)) {
-			return value;
-		}
+function getErrorMessage(error?: any): string {
+	
+	if (error instanceof Error) {
+		return error.message;
+	} else if (typeof error === "string") {
+		return error;
+	} else {
+		return "An unknown Error occurred";
 	}
-	return undefined;
 }
