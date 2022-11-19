57fc94b419a86ec6cd98ddaef21a5015c4c76967
WiggelMc <kim.hh@outlook.de>
2022-11-19 23:08:46 +0100

    fix implementation of objectProperties

--- server/src/util/object.ts
+++ server/src/util/object.ts
@@ -2,14 +2,24 @@ export function objectKeys<E extends object>(e: E): (keyof E & string)[] {
     return Object.keys(e) as (keyof E & string)[];
 }
 
-export function objectProperties<E extends object>(e: E): ((keyof E | 'constructor') & string)[] {
-    return Object.getOwnPropertyNames(Object.getPrototypeOf(e)) as ((keyof E | 'constructor') & string)[];
-}
-
-export function objectKeysAndProperties<E extends object>(e: E): ((keyof E | 'constructor') & string)[] {
-    return objectProperties(e).concat(objectKeys(e));
-}
-
 export function objectValues<E extends object>(e: E): E[keyof E][] {
     return objectKeys(e).map(k => e[k]);
+}
+
+export function ownObjectProperties<E extends object>(e: E): (keyof E & string)[] {
+    return Object.getOwnPropertyNames(e) as (keyof E & string)[];
+}
+
+export function objectProperties<E extends object>(e: E): (keyof E & string)[] {
+    const objectPrototype = ownObjectProperties(Object.prototype);
+    const methods: (keyof E & string)[] = [];
+    let obj = e;
+    
+    while (obj !== null) {
+        const properties = ownObjectProperties(obj).filter(p => !(p in objectPrototype));
+        methods.push(...properties);
+        obj = Object.getPrototypeOf(obj);
+    }
+
+    return [...new Set(methods)];
 }
